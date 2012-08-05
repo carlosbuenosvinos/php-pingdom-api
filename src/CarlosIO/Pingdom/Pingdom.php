395d22acc6ee491ef6c70ef3f199cffb3094258a
Carlos Buenosvinos <carlos@quepimquepam.com>
2012-08-05 05:27:59 -0400

    Refactor + documentation

--- src/CarlosIO/Pingdom/Pingdom.php
+++ src/CarlosIO/Pingdom/Pingdom.php
@@ -4,25 +4,112 @@ namespace CarlosIO\Pingdom;
 use CarlosIO\Pingdom\Check;
 use Guzzle\Service\Client;
 
-class Pingdom
+class Client
 {
-    private $user;
+    const URL_REST = 'https://api.pingdom.com/api/2.0';
+
+    /**
+     * @var string Account username
+     */
+    private $username;
+
+    /**
+     * @var string Account password
+     */
     private $password;
+
+    /**
+     * @var string Account API token (32 chars)
+     */
     private $token;
 
-    public function __construct($user, $password, $token)
+    /**
+     * Builds a Pingdom Client
+     *
+     * @param string $username
+     * @param string $password
+     * @param string $token
+     */
+    public function __construct($username, $password, $token)
     {
-        $this->user = $user;
+        $this->username = $username;
         $this->password = $password;
         $this->token = $token;
     }
 
+    /**
+     * Returns username account
+     *
+     * @return string Username
+     */
+    public function getUsername()
+    {
+        return $this->username;
+    }
+
+    /**
+     * Returns password account
+     *
+     * @return string Password
+     */
+    public function getPassword()
+    {
+        return $this->password;
+    }
+
+    /**
+     * Returns token account
+     *
+     * @return string Token (32 chars)
+     */
+    public function getToken()
+    {
+        return $this->token;
+    }
+
+    /**
+     * Sets username account
+     *
+     * @param string username
+     */
+    public function setUsername($username)
+    {
+        $this->username = $username;
+    }
+
+    /**
+     * Sets password account
+     *
+     * @param string password
+     */
+    public function setPassword($password)
+    {
+        $this->password = $password;
+    }
+
+    /**
+     * Sets token account
+     *
+     * @param string Token (32 chars)
+     */
+    public function setToken($token)
+    {
+        $this->token = $token;
+    }
+
+    /**
+     * Returns a list overview of all checks
+     *
+     * @throws \Exception
+     * @return array<\CarlosIO\Pingdom\Checks> All checks
+     */
     public function getChecks()
     {
-        $client = new Client('https://api.pingdom.com/api/2.0');
+        $client = new Client(static::URL_REST);
 
+        /** @var $request \Guzzle\Http\Message\Request */
         $request = $client->get('checks', array('App-Key' => $this->token));
-        $request->setAuth($this->user, $this->password);
+        $request->setAuth($this->username, $this->password);
         $response = $request->send();
 
         // Execute the request and decode the json result into an associative array
