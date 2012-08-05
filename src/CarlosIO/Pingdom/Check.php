51ee5e543408351751619e3cae6539fb416b0a3e
Carlos Buenosvinos <carlos@quepimquepam.com>
2012-08-05 13:26:21 +0200

    Add mockery as Dev dependency

--- src/CarlosIO/Pingdom/Check.php
+++ src/CarlosIO/Pingdom/Check.php
@@ -3,8 +3,19 @@ namespace CarlosIO\Pingdom;
 
 class Check
 {
+    /**
+     * @var string Check name
+     */
     private $_name;
+
+    /**
+     * @var string Check status
+     */
     private $_status;
+
+    /**
+     * @var \CarlosIO\Pingdom\Account Parent account
+     */
     private $_account;
 
     public function __construct($name, $status, \CarlosIO\Pingdom\Account $account)
@@ -14,16 +25,31 @@ class Check
         $this->_account = $account;
     }
 
+    /**
+     * Returns check name
+     *
+     * @return string Check name
+     */
     public function getName()
     {
         return $this->_name;
     }
 
+    /**
+     * Returns check status
+     *
+     * @return string Check status
+     */
     public function getStatus()
     {
         return $this->_status;
     }
 
+    /**
+     * Returns parent account
+     *
+     * @return \CarlosIO\Pingdom\Account Parent account
+     */
     public function getAccount()
     {
         return $this->_account;
